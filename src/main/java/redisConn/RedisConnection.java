5f089aa392813629111edfbf3a4a2001f76ebaa5
vihar raul <vihar raul>
2017-02-10 22:09:21 -0500

    Create schema & object, Delete, Search Partially complete

--- src/main/java/redisConn/RedisConnection.java
+++ src/main/java/redisConn/RedisConnection.java
@@ -9,6 +9,7 @@ public class RedisConnection {
 	public RedisConnection(){
 		this.jedis = new Jedis("localhost");
 		System.out.println("Ping " + jedis.ping().toString());
+		this.jedis.flushDB();
 	}
 	
 	public Jedis getJedis(){
--- src/main/java/samplApi/CRUDController.java
+++ src/main/java/samplApi/CRUDController.java
@@ -1,5 +1,7 @@
 package samplApi;
 
+import java.util.HashMap;
+
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
@@ -8,6 +10,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,15 +31,54 @@ public class CRUDController {
 	
 	@Autowired
 	RedisConnection redisConnection;
-	
+		
 	@RequestMapping(value = "/{uriType}", method = RequestMethod.POST, consumes = "application/json")
-	public ResponseEntity<Object> create(@RequestHeader HttpHeaders headers, @RequestBody String entity) throws ParseException{
+	public ResponseEntity<Object> create(@RequestHeader HttpHeaders headers, @RequestBody String entity, @PathVariable String uriType) throws ParseException{
 		
 		JSONObject object = (JSONObject) new JSONParser().parse(entity);
 		
+		HashMap hm = new HashMap();
+		hm = (HashMap) new JSONParser().parse(object.toString());
+		
+		for(Object o : hm.keySet()){
+		System.out.println(o + "/"+hm.get(o));
+		}
+		
+		if(hm.containsKey("$schema"))
+			redisConnection.getJedis().set("schema^" + object.get("$schema").toString(), object.toString());
+		
+		if(hm.containsKey("_id"))
 		redisConnection.getJedis().set(object.get("_id").toString(), object.toString());
 			
 		return new ResponseEntity<Object>(object, HttpStatus.CREATED);
+	}
+	
+	@RequestMapping(value = "/{uriType}/{id}", method = RequestMethod.GET, produces = "application/json")
+	public ResponseEntity<Object> get(@RequestHeader HttpHeaders headers, @PathVariable String uriType,
+			@PathVariable String id) throws ParseException{
+		String result = redisConnection.getJedis().get(id);
+		HashMap hm = new HashMap();
+		if(result != null)
+		hm = (HashMap) new JSONParser().parse(result);
+				
+		return new ResponseEntity<Object>(result == null ? null : (JSONObject) new JSONParser().parse(result),result == null ? HttpStatus.FOUND :HttpStatus.NOT_FOUND);		
+	}
+	
+	@RequestMapping(value = "/{uriType}/{id}", method = RequestMethod.DELETE, produces = "application/json")
+	public ResponseEntity<Void> delete(@RequestHeader HttpHeaders headers, @PathVariable String uriType,
+			@PathVariable String id) throws ParseException{
+		
+		
+		String result = redisConnection.getJedis().get(id);
+		if(result == null) 
+		return new ResponseEntity<Void>(HttpStatus.NOT_FOUND);
+		
+		HashMap hm = new HashMap();
+		hm = (HashMap) new JSONParser().parse(result);
+		
+		if(hm.containsKey("_id")) 
+			redisConnection.getJedis().del(id);
+		return new ResponseEntity<>(HttpStatus.OK);
 	}
 
 }
